21abdd5f0796df9d860d95403af42ce7b1ba1edb
AnA <1157559178@qq.com>
2024-04-22 02:57:39 +0800

    build home page and develop page ver0.0.001 Demo

---
+++ js/homeScript.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+    strings: [
+        "Student",
+        "Full Stack Developer",
+        "Bilibili User",
+        "TikTok User",
+        "CSDN uploader",
+        "ZhiHu uploader",
+        "Instagram User"
+    ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
