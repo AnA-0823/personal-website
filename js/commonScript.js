808702bc99232f4a862a7c281089e3ff546018b4
AnA <1157559178@qq.com>
2024-04-23 11:57:05 +0800

    添加导航栏主题按钮 更改导航栏动画 添加主题切换弹窗 ver0.0.004

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,11 +1,23 @@
 let whiteTheme = document.querySelector(".white")
 let blackTheme = document.querySelector(".black")
 let html = document.querySelector("html")
+let themes = document.querySelector(".themes")
+let popup = document.querySelector(".popup")
 
-// if (localStorage.theme) {
-//     html.classList.remove('black-theme', 'white-theme')
-//     html.classList.add(localStorage.theme)
-// }
+if (localStorage.theme) {
+    html.classList.remove('black-theme', 'white-theme')
+    html.classList.add(localStorage.theme)
+}
+
+themes.addEventListener('click', function () {
+    if (themes.classList.contains('active')){
+        themes.classList.remove('active')
+        popup.classList.remove('active')
+    }else{
+        themes.classList.add('active')
+        popup.classList.add('active')
+    }
+})
 
 whiteTheme.addEventListener('click', function () {
     html.classList.remove('black-theme')
